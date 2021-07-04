871a0c23b50ebf108708ed29d0db4323a6d1701c
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-04 19:43:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (driversArray, stringToMatch) {
+    return driversArray.filter(
+        driver => driver.toLowerCase() === stringToMatch.toLowerCase()
+    )
+}
+
+function fuzzyMatch (driversArray, stringToMatch) {
+    return driversArray.filter(
+        driver => driver.startsWith(stringToMatch)
+    )
+}
+
+function matchName (driversArray, stringToMatch) {
+    return driversArray.filter(
+        driver => driver.name === stringToMatch
+    )
+}
